1a070e186230c4e17e40f2c0b50c8b23542c28c7
eileenmx <46695738+eileenmx@users.noreply.github.com>
2019-01-28 18:41:34 -0600

    Add files via upload

---
+++ phpBasics.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>wdv341 | php basics</title>
+</head>
+
+<body>
+
+<?php 
+	$yourName="<h1>Eileen</h1>" //1 & 2.create variable yourName & display in h1
+		?>
+		
+<h2><?php echo $yourName ?></h2> <!--3. display yourName in h2-->
+
+<?php						//4. create following variables
+		$number1=22;
+		$number2=2;
+		$total=$number1 + $number2;	//5. display value of each in the total when added
+
+	
+		?>
+		
+	<?php
+		$array1=array("PHP", "HTML", "JavaScript"); //6. create an array
+	?>
+		
+		
+	<script>
+		
+		//var array1=["PHP", "HTML", "JavaScript"]; 
+	
+	</script>	
+	
+
+</body>
+</html>
