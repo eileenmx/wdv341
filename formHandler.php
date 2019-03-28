64a97ced62cb90e90f9c7c56bca31e933337b410
eileenmx <46695738+eileenmx@users.noreply.github.com>
2019-03-28 13:25:57 -0500

    Add files via upload

--- formHandler.php
+++ formHandler.php
@@ -19,16 +19,32 @@
 	$inFirstName = $_POST["firstName"];		//Get the value entered in the first name field
 	$inLastName = $_POST["lastName"];		//Get the value entered in the last name field
 	$inSchool = $_POST["school"];			//Get the value entered in the school field
+	$inGender = $_POST["radio"];
+	$inClass = $_POST["checkbox"];
+	$inDegree = $_POST["select"];
 	
 
 ?>
 <!DOCTYPE html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>wdv341 | form handler</title>
+<link href="../../../../Users/eileenmcmanus/website/css/websitess.css" rel="stylesheet" type="text/css" style="text/css">
+        <link href="../../../../Users/eileenmcmanus/website/css/head.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
+<div class="head">
+	<h1><a href="index.html">Eileen<br />McManus</a></h1>
+	<ul>
+	<li><a href="about.html">about</a></li>
+	<li><a href="portfolio.html">portfolio</a></li>
+	<li><a href="hw/hw.html">homework</a></li>
+	</ul>
+</div>	
+       	
+<div class="main-single"> 
 <h1>WDV341 Intro PHP</h1>
 <h2>Form Handler Result Page</h2>
 <p>This page displays the results of the Server side processing. </p>
@@ -44,10 +60,20 @@
 	</table>
 </p>
 <h3>Display the values from the form using Method 2. Displays the individual values.</h3>
-<p>School: <?php echo $inSchool; ?></p>
+
 <p>First Name: <?php echo $inFirstName; ?></p>
 <p>Last Name: <?php echo $inLastName; ?></p>
+<p>School: <?php echo $inSchool; ?></p>
+<p>Gender: <?php echo $inGender; ?></p>
+<p>Class Format: <?php echo $inClass; ?></p>
+<p>Degree Type: <?php echo $inDegree; ?></p>
 
-
+	</div>
+		  <footer>
+	  <p>Write | mcmanuseileen790@gmail.com<br />
+	  	
+	  	Call | 515.783.8159<br />
+	  	Link | <a href="http://www.linkedin.com/in/eileen-mcmanus" target="_blank">linkedin.com/eileen-mcmanus</a></p>
+	  </footer>
 </body>
 </html>
